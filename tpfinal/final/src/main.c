e2e42d244f81d04d9249c586e6545d2ab166a2f5
Nicolas Piñera <nicolaspinera@gmail.com>
2025-10-12 11:05:08 -0300

    Actualizacion main

--- tpfinal/final/src/main.c
+++ tpfinal/final/src/main.c
@@ -2,122 +2,83 @@
 #include "lpc17xx_pinsel.h"
 #include "lpc17xx_gpio.h"
 #include "lpc17xx_exti.h"
-#define DELAY 5000000
 
-volatile int banderaFin = 0;
-volatile int banderaComienzo = 0;
-//1.31 para dar tension
-// 2.10 para EINT0 -> flanco descendente
+// MACROS Y DEFINE
+#define FUNC_0 0
+#define FUNC_1 1
+#define FUNC_2 2
+#define FUNC_3 3
 
+// Estructura para configuracion sencilla de puertos
 typedef struct {
-	uint8_t port;
+	uint8_t puerto;
 	uint32_t pin;
-} Led_t;
+	uint8_t func;
+} Pines;
 
 // Definimos los LEDs en un arreglo
-Led_t leds[] = { { 2, 12 }, // LED1
-		{ 2, 13 }, // LED2
-		{ 2, 11 }, // LED3
-		{ 0, 21 },  // LED4
-		{ 0, 3 }, // LED 5
-		{ 0, 2 }, // LED 6
+Pines pines_uso[] = { { 2, 12, FUNC_0 }, // P2.12 - Funcion GPIO
+		{ 2, 13, FUNC_0 }, // P2.13 - Funcion GPIO
+		{ 2, 11, FUNC_0 }, // P2.11 - Funcion GPIO
+		{ 0, 21, FUNC_0 },  // P0.21 - Funcion GPIO
+		{ 0, 3, FUNC_0 }, // P0.3 - Funcion GPIO
+		{ 0, 2, FUNC_0 }, // P0.2 - Funcion GPIO
 		};
-const int NUM_LEDS = sizeof(leds) / sizeof(leds[0]);
 
-void delay(void) {
-	for (int i = 0; i < DELAY; i++)
-		;
-}
+// Calculo del numero de pines
+const int NUM_PINES = sizeof(leds) / sizeof(leds[0]);
 
-void prenderTodos(void) {
-	for (int i = 0; i < NUM_LEDS; i++) {
-		GPIO_SetValue(leds[i].port, 1 << leds[i].pin);
-	}
-}
-
-void apagarTodos(void) {
-	for (int i = 0; i < NUM_LEDS; i++) {
-		GPIO_ClearValue(leds[i].port, 1 << leds[i].pin);
-	}
-}
-
-void desplazamiento(void) {
-	// ida
-	for (int i = 0; i < NUM_LEDS; i++) {
-		apagarTodos();
-		GPIO_SetValue(leds[i].port, 1 << leds[i].pin);
-		delay();
-	}
-	// vuelta
-	for (int i = NUM_LEDS - 1; i >= 0; i--) {
-		apagarTodos();
-		GPIO_SetValue(leds[i].port, 1 << leds[i].pin);
-		delay();
-	}
-	banderaFin = 1;
-}
-
-void EINT0_IRQHandler() {
-	banderaComienzo ^= 1;
-	// banderaComienzo = 1;
-	EXTI_ClearEXTIFlag(0); // limpia bandera
-}
+void configPIN(void); // Configuracion de GPIO
+void configADC(void); // Configuracion del ADC
+void configDAC(void); // configuracion del DAC
+void configDMA(void); // Configuracion de DMA
+void configUART(void); // Configuracion de comunicacion UART
 
 int main(void) {
 	SystemInit();
+	configPIN();
+	while (1) {
+	}
+}
 
+void configPIN(void) {
 	// Configuración de pines
 	PINSEL_CFG_Type pin;
-	for (int i = 0; i < NUM_LEDS; i++) {
-		pin.Portnum = leds[i].port;
-		pin.Pinnum = leds[i].pin; // obtiene el número del pin
-		pin.Funcnum = 0;
+	for (int i = 0; i < NUM_PINES; i++) {
+		pin.Portnum = pines_uso[i].puerto;
+		pin.Pinnum = pines_uso[i].pin;
+		pin.Funcnum = pines_uso[i].func;
 		pin.Pinmode = 0;
 		pin.OpenDrain = 0;
 		PINSEL_ConfigPin(&pin);
-
-		GPIO_SetDir(leds[i].port, 1 << leds[i].pin, 1);
-	}
-	pin.Portnum = 1;
-	pin.Pinnum = 31;
-	pin.Funcnum = 0;
-	pin.Pinmode = 0;
-	pin.OpenDrain = 0;
-	PINSEL_ConfigPin(&pin);
-	pin.Portnum = 2;
-	pin.Pinnum = 10; // EINT0
-	pin.Funcnum = 1;
-	pin.Pinmode = 0;
-	pin.OpenDrain = 0;
-	PINSEL_ConfigPin(&pin);
-	LPC_PINCON->PINMODE4 &= ~(1 << 20);
-	LPC_PINCON->PINMODE4 |= (1 << 21);
-
-	// EXTI_InitTypeDef externa;
-	// externa.EXTI_Line = 0;
-	// externa.EXTI_Mode = 1;
-	// externa.EXTI_polarity = 0; // bajada
-	// EXTI_Config(&externa); // Configuracion
-	// EXTI_SetMode(0, 1);
-	// EXTI_SetPolarity(0, 0);
-
-	//NVIC_EnableIRQ(EINT0_IRQn); // habilito
-
-	while (1) {
-		if (banderaComienzo) {
-			desplazamiento();
-			if (banderaFin) {
-				for (int i = 0; i < 3; i++) {
-					prenderTodos();
-					delay();
-					apagarTodos();
-					delay();
-				}
-				banderaFin = 0;
-				// banderaComienzo = 0;
-			}
-		} else {
-			apagarTodos();
-		}
 	}
 }
+/*
+ * void prenderTodos(void) {
+ for (int i = 0; i < NUM_LEDS; i++) {
+ GPIO_SetValue(leds[i].port, 1 << leds[i].pin);
+ }
+ }
+
+ void apagarTodos(void) {
+ for (int i = 0; i < NUM_LEDS; i++) {
+ GPIO_ClearValue(leds[i].port, 1 << leds[i].pin);
+ }
+ }
+
+ void desplazamiento(void) {
+ // ida
+ for (int i = 0; i < NUM_LEDS; i++) {
+ apagarTodos();
+ GPIO_SetValue(leds[i].port, 1 << leds[i].pin);
+ delay();
+ }
+ // vuelta
+ for (int i = NUM_LEDS - 1; i >= 0; i--) {
+ apagarTodos();
+ GPIO_SetValue(leds[i].port, 1 << leds[i].pin);
+ delay();
+ }
+ banderaFin = 1;
+ }
+ * */

92eb597eaee58760fe75ddac57ad9cb8b79d4e8b
NicoPiñera <nicolaspinera@gmail.com>
2025-10-23 12:12:37 -0300

    Actualizar menú del osciloscopio para incluir opción de establecer frecuencia y corregir flujo de salida

--- src/main.py
+++ src/main.py
@@ -1,18 +1,49 @@
+import serial as s #PySerial para la comunicacion UART
+import os
+os.system('clear')
 
 def menu():
     opcion = 0
     print("--- Menu Osciloscopio --- \n")
-    print("1) Muestrear señales")
-    print("2) Generar señal Seno")
-    print("3) Generar señal rampa")
-    print("4) Salir \n")
-    opcion = int(input("Ingrese opcion [1,2,3,4]: "))
+    print("1) Establecer Frecuencia")
+    print("2) Muestrear señales")
+    print("3) Generar señal Seno")
+    print("4) Generar señal rampa")
+    print("5) Salir \n")
+    opcion = int(input("Ingrese opcion [1,2,3,4,5]: "))
     return opcion
+
+def establecerFrecuencia():
+    frec = int(input("Frecuencia en Hz: "))
+    print(f"Estableciendo frecuencia en {frec}[Hz]")
+
+def muestrear():
+    print("Muestreando señal")
+
+def senalSeno():
+    print("Generando Señal Seno")
+
+def senalRampa():
+    print("Generando señal Rampa")
+    
+
 
 
 while(1):
     op = menu()
-    if(op == 4):
-        print("Saliendo")
-        break
+    if(op == 1):
+        establecerFrecuencia()
+        continue
+    elif op==2 :
+        muestrear()
+        continue
+    elif op == 3:
+        senalSeno()
+        continue
+    elif op == 4:
+        senalRampa()
+        continue
+    elif op == 5:
+        print("--- Saliendo ---")
+        break 
     

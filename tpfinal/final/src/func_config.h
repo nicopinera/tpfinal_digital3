fe823120edbaa1573e228cdd33d5fd553f3d50f5
NicoPiñera <nicolaspinera@gmail.com>
2025-10-23 09:12:16 -0300

    Configuracion del ADC

--- tpfinal/final/src/func_config.h
+++ tpfinal/final/src/func_config.h
@@ -7,11 +7,14 @@
 #define FUNC_CONFIG_H_
 #include <stdint.h>
 // MACROS Y DEFINE
-#define FUNC_0 0
-#define FUNC_1 1
-#define FUNC_2 2
-#define FUNC_3 3
+#define FUNC_0 0 // funcion 0
+#define FUNC_1 1 // funcion 1
+#define FUNC_2 2 // funcion 2
+#define FUNC_3 3 // funcion 3
 #define TIME_DELAY 1000000
+#define MATH_REG_1 // valor del match register
+#define PR_TICK_1 1 // valor en ticks del pre scaler 1
+#define MAX_SAMPLES 100
 
 // Estructura para configuracion sencilla de puertos
 typedef struct {
--- tpfinal/final/src/main.c
+++ tpfinal/final/src/main.c
@@ -1,3 +1,24 @@
+/*Proyecto: Mini Osciloscopio + Generador de señales
+
+Idea: La PC puede pedir al LPC1769 que:
+
+- Lea una señal analógica (ADC con DMA): si se utiliza la opcion de captuar una señal utilizaria con el adc utilizo un canal de DMA y dos listas enlazadas
+para ir guardando datos. El adc se triggerea con el timer para settear una frecuencia de muestreo.
+
+- Genere una onda por DAC seno o rampa (también vía DMA)
+
+En la PC se muestra la señal capturada o generada (por ejemplo en Python). Qué integra
+
+Requisito: Uso
+
+    ADC: Captura la señal
+    DAC: Reproduce la señal o genera una que se vera en un osciloscopio
+    DMA: Mueve buffers ADC ↔ memoria ↔ DAC
+    Timer: Controla frecuencia de muestreo
+    UART: Intercambia comandos y muestras con la PC
+ *
+ * */
+
 #include "LPC17xx.h"
 #include "lpc17xx_pinsel.h"
 #include "lpc17xx_gpio.h"
@@ -7,9 +28,11 @@
 
 // Definimos los Pines en un arreglo
 Pines pines_uso[] = { { 0, 22, FUNC_0 }, // P2.12 - Funcion GPIO
-		{ 3, 25, FUNC_0 }, // P2.13 - Funcion GPIO
-		{ 3, 26, FUNC_0 }, // P2.11 - Funcion GPIO
+		{0,23,FUNC_1}, // AD0.0 para capturar la señal
 		};
+
+uint16_t buffer1[MAX_SAMPLES]; // buffer 1 para muestras del ADC
+uint16_t buffer2[MAX_SAMPLES]; // buffer 2 para muestras del ADC
 
 // Calculo del numero de pines
 const int NUM_PINES = sizeof(pines_uso) / sizeof(pines_uso[0]);
@@ -20,31 +43,13 @@ void configDAC(void); // configuracion del DAC
 void configDMA(void); // Configuracion de DMA
 void configUART(void); // Configuracion de comunicacion UART
 
-
-
-void apagar() {
-	for (int i = 0; i < NUM_PINES; i++) {
-		GPIO_SetValue(pines_uso[i].puerto, 1 << pines_uso[i].pin);
-
-	}
-}
-
 int main(void) {
 	SystemInit();
 	configPIN();
-	//configADC();
+	configADC();
 
 	while (1) {
-		apagar();
-		GPIO_ClearValue(0, 1 << 22);
-		delay();
-		apagar();
-		GPIO_ClearValue(3, 1 << 25);
-		delay();
-		apagar();
-		GPIO_ClearValue(3, 1 << 26);
-		delay();
-
+		GPIO_ClearValue(pines_uso[0].puerto, 1 << pines_uso[0].pin); // prendo led rojo
 	}
 }
 
@@ -61,32 +66,11 @@ void configPIN(void) {
 		GPIO_SetDir(pines_uso[i].puerto, 1 << pines_uso[i].pin, 1);
 	}
 }
-/*
- * void prenderTodos(void) {
- for (int i = 0; i < NUM_LEDS; i++) {
- GPIO_SetValue(leds[i].port, 1 << leds[i].pin);
- }
- }
 
- void apagarTodos(void) {
- for (int i = 0; i < NUM_LEDS; i++) {
- GPIO_ClearValue(leds[i].port, 1 << leds[i].pin);
- }
- }
+void configADC(void){
+	ADC_Init(LPC_ADC, 200000);
+	ADC_StartCmd(LPC_ADC, ADC_START_ON_MAT01); // INICIA CON EL TIMER 0 - MATCH 1
+	ADC_EdgeStartConfig(LPC_ADC, ADC_START_ON_RISING); // CADA FLANCO DE SUBIDA
+	ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_0, ENABLE); // HABILITO CANAL 0
+}
 
- void desplazamiento(void) {
- // ida
- for (int i = 0; i < NUM_LEDS; i++) {
- apagarTodos();
- GPIO_SetValue(leds[i].port, 1 << leds[i].pin);
- delay();
- }
- // vuelta
- for (int i = NUM_LEDS - 1; i >= 0; i--) {
- apagarTodos();
- GPIO_SetValue(leds[i].port, 1 << leds[i].pin);
- delay();
- }
- banderaFin = 1;
- }
- * */

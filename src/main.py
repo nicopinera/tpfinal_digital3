1931fb25b6dc20409276285174c54a301d9d61db
Nicolas Piñera <nicolaspinera@gmail.com>
2025-10-23 21:14:50 -0300

    config menu

--- src/main.py
+++ src/main.py
@@ -9,25 +9,14 @@ import os
 import config
 os.system('cls')
 
-'''
-Cuando nosotros transmitimos usando UART enviamos todos los caractres del comando
-En C nosotros tomamos estos caracteres que nos llegan y los almacenamos hasta encontrar un \n
-'''
+
+# Cuando nosotros transmitimos usando UART enviamos todos los caractres del comando
+# En C nosotros tomamos estos caracteres que nos llegan y los almacenamos hasta encontrar un \n
+
 
 lpc_uart = s.Serial(config.puerto,config.baudios,timeout=1) # abro el puerto, le digo que espere 1 segundo
 data_buffer = deque(maxlen=config.sample) # creamos un buffer circular de 100 muestras
-time.sleep(2) # duermo para que se termine de configurar todo
-
-def menu():
-    opcion = 0
-    print("--- Menu Osciloscopio --- \n")
-    print("1) Establecer Frecuencia")
-    print("2) Muestrear señales")
-    print("3) Generar señal Seno")
-    print("4) Generar señal rampa")
-    print("5) Salir \n")
-    opcion = int(input("Ingrese opcion [1,2,3,4,5]: "))
-    return opcion
+# time.sleep(2) # duermo para que se termine de configurar todo
 
 # Esta funcion correra en un hilo demonio
 def recibirDatos():
@@ -38,12 +27,13 @@ def recibirDatos():
                 val_uint16 = struct.unpack('<H',lectura)[0] # se convierte a un uint16_t
                 data_buffer.append(val_uint16)
 
-threading.Thread(target=recibirDatos,daemon=True).start() # creamos un hilo deamon que ejecuta recibirDatos 
+hilo = threading.Thread(target=recibirDatos,daemon=True) # creamos un hilo deamon que ejecuta recibirDatos 
+hilo.start()
 
 # configuracion del plot
 plt.ion() # modo interactivo
 fig,ax = plt.subplots()
-line,=ax.prot([],[])
+line,=ax.plot([],[])
 ax.set_ylim(0,2**12)
 ax.set_xlim(0,config.sample)
 
@@ -55,37 +45,40 @@ def actualizarPlot():
         plt.pause(0.001)
         fig.canvas.draw()
 
-def establecerFrecuencia():
-    frec = int(input("Frecuencia en Hz: "))
-    print(f"Estableciendo frecuencia en {frec}[Hz]")
+def enviar_Comandos(cmd):
+    lpc_uart.write((cmd + '\n').encode())
+    print(f"Osc> Enviado: {cmd}")
 
-def muestrear():
-    print("Muestreando señal")
+def menu():
+    while 1:
+        print("--- Osciloscopio ---")
+        print("Osc> 1) Establecer frecuencia")
+        print("Osc> 2) Iniciar conversion")
+        print("Osc> 3) Detener conversion")
+        print("Osc> 4) Generar Seno por Software")
+        print("Osc> 5) Generar Rampa por Software")
+        print("Osc> 6) Salir")
+        op = int(input("Osc> Ingrese una opcion [1,2,3,4,5,6]: \nOsc>"))
+        if op == 1:
+            frec = input("Frecuencia en Hz: ")
+            print(f"Osc> Estableciendo frecuencia en {frec}[Hz]")
+            enviar_Comandos(frec)
+        elif op == 2:
+            config.muestreando = True
+            enviar_Comandos("START_ADC")
+        elif op == 3:
+            config.muestreando = False
+            enviar_Comandos("STOP_ADC")
+        elif op == 4:
+            enviar_Comandos("GEN_SIN")
+        elif op == 5:
+            enviar_Comandos("GEN_RAM")
+        elif op == 6:
+            print("Osc> Saliendo")
+            lpc_uart.close() # cierro puerto serial
+            plt.close('all') # cierro el plot
+            break
 
-def senalSeno():
-    print("Generando Señal Seno")
+if __name__ == "__main__":
+    menu()
 
-def senalRampa():
-    print("Generando señal Rampa")
-
-# Loop principal
-while(1):
-    op = menu()
-    if(op == 1):
-        establecerFrecuencia()
-        continue
-    elif op==2 :
-        muestrear()
-        continue
-    elif op == 3:
-        senalSeno()
-        continue
-    elif op == 4:
-        senalRampa()
-        continue
-    elif op == 5:
-        print("--- Saliendo ---")
-        break
-    else:
-        print("Opcion invalida")
-        
